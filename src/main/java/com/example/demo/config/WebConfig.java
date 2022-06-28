b60ee335588fca009f0f815084c6bd2207e80152
Cleston Sousa <cleston.sousa@capgemini.com>
2022-06-28 11:53:04 -0300

    external file properties config

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,10 +1,13 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@PropertySource("classpath:application.properties")
+@PropertySource("file:/test/poc/application.properties")
 public class WebConfig extends WebSecurityConfigurerAdapter {
 
   // @Autowired
